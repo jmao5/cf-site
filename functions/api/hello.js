10920fe7a9a95706c72743bd48c10f84c0f6809b
jmao5 <jsh5890@kakao.com>
2023-12-12 19:31:52 +0900

    first commit

---
+++ functions/api/hello.js
@@ -0,0 +1,5 @@
+// functions/api/hello.js
+
+export function onRequest(ctx) {
+  return new Response("Hello World!");
+}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold flex justify-center items-center p-8">
+        Welcome to React with Pages + Workers + D1 Example
+      </div>
+    </>
+  );
+}
+
+export default App;
